c3749ae335ffb627f9dae55b375c5cb3ca7e42e5
joaowartins <joaootavioaraujomartins@gmail.com>
2025-11-05 15:15:20 -0300

    feat: adiciona server actions para CRUD de categorias

--- lib/prisma-client.ts
+++ lib/prisma-client.ts
@@ -1,3 +1,14 @@
 import { PrismaClient } from "@/generated/prisma/client";
 
-const prisma = new PrismaClient()
+declare global {
+	// allow global `var` to avoid TS errors in development where module reloads
+	// can create multiple PrismaClient instances
+	// eslint-disable-next-line no-var
+	var __prisma_client__: PrismaClient | undefined;
+}
+
+const prisma = global.__prisma_client__ ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") global.__prisma_client__ = prisma;
+
+export default prisma;
